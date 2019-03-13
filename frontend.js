93265152977edb253a526d6f223b2c7b7d314a86
Utkarsh Gupta <utkarshg99@gmail.com>
2019-03-13 14:54:07 +0530

    conversion to vue js has begun

---
+++ frontend.js
@@ -0,0 +1,71 @@
+var config = {
+    apiKey: "AIzaSyA8EgDnBAxzf_9uYp0G81OBf0JJc0ZDcug",
+    authDomain: "ubaiitksite.firebaseapp.com",
+    databaseURL: "https://ubaiitksite.firebaseio.com",
+    projectId: "ubaiitksite",
+    storageBucket: "ubaiitksite.appspot.com",
+    messagingSenderId: "41170240626"
+  };
+  firebase.initializeApp(config);
+  var db = firebase.firestore();
+  var carouselvue = new Vue({
+    el: '#carousel-vue',
+    data () {
+      return {
+        carcont: []
+      }
+    },
+    mounted () {
+      db.collection("central slider images").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.carcont.push(doc.data());
+        });
+      });
+    }
+  });
+  var themevue = new Vue({
+    el: '#themesvue',
+    data () {
+      return {
+        theme: []
+      }
+    },
+    mounted () {
+      db.collection("themes").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.theme.push(doc.data());
+        //   console.log(this.theme);
+        });
+      });
+    }
+  });
+  var inspire = new Vue({
+    el: '#inspiration-vue',
+    data () {
+      return {
+        inspirations: []
+      }
+    },
+    mounted () {
+      db.collection("home page inspiration images").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.inspirations.push(doc.data());
+        });
+      });
+    }
+  });
+  var news = new Vue({
+    el: '#news-vue',
+    data () {
+      return {
+        newscontent: []
+      }
+    },
+    mounted () {
+      db.collection("").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.newscontent.push(doc.data());
+        });
+      });
+    }
+  });
